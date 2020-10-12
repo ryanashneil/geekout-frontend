07b278a6240caab44d9fa74d7a9f770c5a694ba0
ryanashneil <ryanashneil@gmail.com>
2020-10-12 09:26:04 +0800

    feat(filter-all): add "Singapore" filter option to list all results

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,18 +2,26 @@ import React, { useEffect, useState } from "react";
 import { getDonations } from "../api";
 import Card from "./Card";
 
+const SINGAPORE = "Singapore";
+
 const Results = () => {
   const [locations, setLocations] = useState([]);
   const [donations, setDonations] = useState([]);
   const [division, setDivision] = useState(undefined);
 
+  useEffect(() => void loadData(), []);
+
   const loadData = async () => {
     const data = await getDonations();
+    const uniqueLocations = Array.from(new Set(data.map((item) => item.division)));
+    setDivision(SINGAPORE);
     setDonations(data);
-    setLocations(Array.from(new Set(data.map((item) => item.division))));
+    setLocations([SINGAPORE, ...uniqueLocations]);
   };
 
-  useEffect(() => void loadData(), []);
+  const onSelect = (e) => {
+    setDivision(e.target.value);
+  };
 
   if (donations.length === 0) {
     return (
@@ -30,17 +38,14 @@ const Results = () => {
       <div className="container">
         <h2>
           Items needed in:
-          <select
-            value={division}
-            onChange={(e) => setDivision(e.target.value)}
-          >
+          <select value={division} onChange={onSelect}>
             {locations.map((location) => (
               <option>{location}</option>
             ))}
           </select>
         </h2>
         {donations
-          .filter((item) => item.division === division)
+          .filter((item) => division === SINGAPORE || item.division === division)
           .map((item) => (
             <Card name={item.name} quantity={item.quantity} unit={item.unit} />
           ))}
